8c1ac3a8e9c728a36ce72e6b4ba34c9321a1b66c
Koen Lageveen <koen.lageveen@me.com>
2018-03-25 20:00:05 +0200

    clean up things that don't work in SL4

--- linter.py
+++ linter.py
@@ -1,29 +1,12 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Written by Aparajita Fishman
-# Copyright (c) 2015-2016 The SublimeLinter Community
-# Copyright (c) 2013-2014 Aparajita Fishman
-#
-# License: MIT
-#
-
-"""This module exports the Coffee plugin class."""
-
 from SublimeLinter.lint import Linter, util
 
 
 class Coffee(Linter):
-    """Provides an interface to coffee --compile."""
-
-    syntax = 'coffeescript'
     cmd = 'coffee --compile --stdio'
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 1.5'
     regex = (
         r'^.+?:(?P<line>\d+):(?P<col>\d+): (?:(?P<error>error)|(?P<warning>warning)): (?P<message>[^\r\n]+)'
     )
     error_stream = util.STREAM_STDERR
-    comment_re = r'\s*/[/*]'
+    defaults = {
+        'selector': 'source.coffee'
+    }
